d1c42942b085de744e9d6c0c8959e6510649fb4e
rnrhea <rn.rhea@gmail.com>
2020-11-01 11:33:31 -0500

    TA edits

--- public/utility_scripts/script_faculty.py
+++ public/utility_scripts/script_faculty.py
@@ -4,12 +4,11 @@ import re
 file1 = open('faculty_names.txt', 'r') 
 Lines = file1.readlines() 
 
-html_1 = '<li> <a href="#"> <table> <tr> <td class="d1">'
+html_1 = '<li> <a href="#"> <table id ="faculty"> <tr> <td class="d1">'
 #then name
-html_2 = '</td> <td class="d3"> <button>'  
+html_2 = '</td> <td class="d3"> <button onclick="selectOffice(5156)" style="font-size: 35px;">'  
 #then room num
 html_3 = '</button> </td> </tr> </table> </a> </li>'
-
 
 for line in Lines:
     m = re.search('(.*) is in IRB (\d\d\d\d).', line)
